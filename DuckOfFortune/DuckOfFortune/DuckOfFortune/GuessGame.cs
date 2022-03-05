448593b03da28f738a145b0d8701660372500b5f
Cameron Sutcliffe <CSutcliffe@dundee.ac.uk>
2022-03-05 15:02:23 +0000

    added game screen

--- DuckOfFortune/DuckOfFortune/DuckOfFortune/GuessGame.cs
+++ DuckOfFortune/DuckOfFortune/DuckOfFortune/GuessGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,49 @@ namespace DuckOfFortune
 {
     public partial class GuessGame : Form
     {
+
+
         public GuessGame()
         {
             InitializeComponent();
+        }
+
+
+        private void GenerateBtn_Click(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines("Phrases.txt");
+            Random rand = new Random();
+            var chosenline = lines[rand.Next(lines.Length)];
+            Console.WriteLine(chosenline);
+            char[] characters = chosenline.ToCharArray();
+            var longy = characters.Length;
+            if (longy <= 52)
+            {
+                var count = 0;
+
+                while (count < longy)
+                {
+
+
+
+                    foreach (Control control in Controls)
+                    {
+                        if (control is TextBox)
+                        {
+                            //Investigate and do your thing
+                        }
+                    }
+
+                    Console.WriteLine("test2");
+                    var letterToWrite = characters[count];
+                  
+
+                    count++;
+                }
+            }
+
+
+
         }
     }
 }
