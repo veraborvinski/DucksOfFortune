be0f32a964d22810f448f9c9b951130f9af340c5
veraborvinski <2421818@dundee.ac.uk>
2022-03-05 17:13:00 +0000

    removing ugly label

--- DuckOfFortune/DuckOfFortune/DuckOfFortune/HighScore.Designer.cs
+++ DuckOfFortune/DuckOfFortune/DuckOfFortune/HighScore.Designer.cs
@@ -37,9 +37,8 @@ namespace DuckOfFortune
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(285, 53);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // HighScore
