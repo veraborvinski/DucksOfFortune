89b55c3b27a351355d07ad3fa786defcd1788afc
Cameron Sutcliffe <CSutcliffe@dundee.ac.uk>
2022-03-05 13:30:04 +0000

    Added forms and adding user name

--- DuckOfFortune/DuckOfFortune/DuckOfFortune/StartScreen.Designer.cs
+++ DuckOfFortune/DuckOfFortune/DuckOfFortune/StartScreen.Designer.cs
@@ -29,7 +29,10 @@
         private void InitializeComponent()
         {
             this.btnConfirm = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.nameLbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnConfirm
@@ -40,24 +43,58 @@
             this.btnConfirm.TabIndex = 0;
             this.btnConfirm.Text = "Confirm";
             this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
             // 
-            // textBox1
+            // nameBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(506, 264);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(115, 26);
-            this.textBox1.TabIndex = 1;
+            this.nameBox.Location = new System.Drawing.Point(506, 264);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(115, 26);
+            this.nameBox.TabIndex = 1;
+            // 
+            // btnYes
+            // 
+            this.btnYes.Location = new System.Drawing.Point(409, 421);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(100, 56);
+            this.btnYes.TabIndex = 2;
+            this.btnYes.Text = "Yes";
+            this.btnYes.UseVisualStyleBackColor = true;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.Location = new System.Drawing.Point(618, 421);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(100, 56);
+            this.btnNo.TabIndex = 3;
+            this.btnNo.Text = "Uh, no?";
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // nameLbl
+            // 
+            this.nameLbl.AutoSize = true;
+            this.nameLbl.Location = new System.Drawing.Point(564, 312);
+            this.nameLbl.Name = "nameLbl";
+            this.nameLbl.Size = new System.Drawing.Size(0, 20);
+            this.nameLbl.TabIndex = 4;
+            this.nameLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // StartScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1178, 844);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.nameLbl);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.nameBox);
             this.Controls.Add(this.btnConfirm);
             this.Name = "StartScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "StartScreen";
+            this.Load += new System.EventHandler(this.StartScreen_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,6 +103,9 @@
         #endregion
 
         private System.Windows.Forms.Button btnConfirm;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Label nameLbl;
     }
 }
--- DuckOfFortune/DuckOfFortune/DuckOfFortune/StartScreen.cs
+++ DuckOfFortune/DuckOfFortune/DuckOfFortune/StartScreen.cs
@@ -8,13 +8,61 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace DuckOfFortune
 {
+    using System.IO;
+    using System.Threading.Tasks;
     public partial class StartScreen : Form
     {
+        private string name;
+
         public StartScreen()
         {
             InitializeComponent();
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            name = nameBox.Text;
+            btnConfirm.Hide();
+            nameBox.Hide();
+            btnYes.Show();
+            btnNo.Show();
+            nameLbl.Show();
+            nameLbl.Text = "Are you sure your name is: " + name;
+
+        }
+
+        private void StartScreen_Load(object sender, EventArgs e)
+        {
+            btnConfirm.Show();
+            nameBox.Show();
+            btnYes.Hide();
+            btnNo.Hide();
+            nameLbl.Hide();
+
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            btnConfirm.Show();
+            nameBox.Show();
+            btnYes.Hide();
+            btnNo.Hide();
+            nameLbl.Hide();
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            string startupPath = System.IO.Directory.GetCurrentDirectory();
+            File.AppendAllText(@"Scores.txt", name + ",0" + Environment.NewLine);
+
+            this.Hide();
+            var guessgame = new GuessGame();
+            guessgame.Closed += (s, args) => this.Close();
+            guessgame.Show();
+
         }
     }
 }
