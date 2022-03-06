20042cd3f2277018b3f1bdd6602d899b4c879bb0
AbdulmalikAdegoke <31307366+AbdulmalikAdegoke@users.noreply.github.com>
2022-03-06 07:30:58 +0000

    changed the designer contents for high score

--- DuckOfFortune/DuckOfFortune/DuckOfFortune/HighScore.Designer.cs
+++ DuckOfFortune/DuckOfFortune/DuckOfFortune/HighScore.Designer.cs
@@ -1,4 +1,4 @@
-﻿
+
 namespace DuckOfFortune
 {
     partial class HighScore
@@ -29,27 +29,97 @@ namespace DuckOfFortune
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HighScore));
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.first = new System.Windows.Forms.Label();
+            this.second = new System.Windows.Forms.Label();
+            this.third = new System.Windows.Forms.Label();
+            this.mainMenuButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(458, 60);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(305, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.Location = new System.Drawing.Point(281, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "HIGH SCORES";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.Location = new System.Drawing.Point(331, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "TOP 3";
+            // 
+            // first
+            // 
+            this.first.AutoSize = true;
+            this.first.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.first.Location = new System.Drawing.Point(333, 168);
+            this.first.Name = "first";
+            this.first.Size = new System.Drawing.Size(20, 17);
+            this.first.TabIndex = 4;
+            this.first.Text = "1.";
+            // 
+            // second
+            // 
+            this.second.AutoSize = true;
+            this.second.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.second.Location = new System.Drawing.Point(333, 212);
+            this.second.Name = "second";
+            this.second.Size = new System.Drawing.Size(20, 17);
+            this.second.TabIndex = 5;
+            this.second.Text = "2.";
+            // 
+            // third
+            // 
+            this.third.AutoSize = true;
+            this.third.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.third.Location = new System.Drawing.Point(333, 268);
+            this.third.Name = "third";
+            this.third.Size = new System.Drawing.Size(20, 17);
+            this.third.TabIndex = 6;
+            this.third.Text = "3.";
+            // 
+            // mainMenuButton
+            // 
+            this.mainMenuButton.Location = new System.Drawing.Point(287, 403);
+            this.mainMenuButton.Name = "mainMenuButton";
+            this.mainMenuButton.Size = new System.Drawing.Size(139, 61);
+            this.mainMenuButton.TabIndex = 7;
+            this.mainMenuButton.Text = "Main Menu";
+            this.mainMenuButton.UseVisualStyleBackColor = true;
+            this.mainMenuButton.Click += new System.EventHandler(this.mainMenuButton_Click);
             // 
             // HighScore
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1178, 844);
+            this.ClientSize = new System.Drawing.Size(785, 549);
+            this.Controls.Add(this.mainMenuButton);
+            this.Controls.Add(this.third);
+            this.Controls.Add(this.second);
+            this.Controls.Add(this.first);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.MaximumSize = new System.Drawing.Size(1200, 900);
-            this.MinimumSize = new System.Drawing.Size(1200, 900);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "HighScore";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "HighScore";
@@ -62,5 +132,11 @@ namespace DuckOfFortune
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label first;
+        private System.Windows.Forms.Label second;
+        private System.Windows.Forms.Label third;
+        private System.Windows.Forms.Button mainMenuButton;
     }
 }
