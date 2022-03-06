277a350028a95d2c2db16886d431f1feeb5eb098
veraborvinski <2421818@dundee.ac.uk>
2022-03-06 03:29:56 +0000

    design win form

--- DuckOfFortune/DuckOfFortune/DuckOfFortune/WinForm.Designer.cs
+++ DuckOfFortune/DuckOfFortune/DuckOfFortune/WinForm.Designer.cs
@@ -33,11 +33,13 @@ namespace DuckOfFortune
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(504, 285);
+            this.button3.Location = new System.Drawing.Point(636, 368);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(114, 49);
             this.button3.TabIndex = 7;
@@ -47,7 +49,7 @@ namespace DuckOfFortune
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(344, 285);
+            this.button2.Location = new System.Drawing.Point(347, 368);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(114, 49);
             this.button2.TabIndex = 6;
@@ -57,7 +59,7 @@ namespace DuckOfFortune
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(182, 285);
+            this.button1.Location = new System.Drawing.Point(41, 368);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(114, 49);
             this.button1.TabIndex = 5;
@@ -68,24 +70,39 @@ namespace DuckOfFortune
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(368, 116);
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(552, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(93, 33);
             this.label1.TabIndex = 4;
             this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::DuckOfFortune.Properties.Resources.bezos_duck;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(41, -5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(351, 367);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
             // 
             // WinForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "WinForm";
             this.Text = "WinForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,5 +114,6 @@ namespace DuckOfFortune
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
