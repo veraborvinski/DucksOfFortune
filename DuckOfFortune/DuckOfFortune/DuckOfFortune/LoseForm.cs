780ac0b101489a7815635b15b9129d0666103d49
veraborvinski <2421818@dundee.ac.uk>
2022-03-06 02:43:49 +0000

    added forms

---
+++ DuckOfFortune/DuckOfFortune/DuckOfFortune/LoseForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuckOfFortune
+{
+    public partial class LoseForm : Form
+    {
+        public LoseForm()
+        {
+            InitializeComponent();
+            label1.Text = "You have lost";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            HighScore highscore = new HighScore();
+            highscore.Show();
+        }
+    }
+}
---
+++ DuckOfFortune/DuckOfFortune/DuckOfFortune/WinForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuckOfFortune
+{
+    public partial class WinForm : Form
+    {
+        public WinForm()
+        {
+            InitializeComponent();
+            label1.Text = "You have won";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            HighScore highscore = new HighScore();
+            highscore.Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
