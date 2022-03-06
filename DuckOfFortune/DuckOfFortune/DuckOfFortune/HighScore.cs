afb21dcb0b3d251577c5a7047cbdfc6e1af69b30
AbdulmalikAdegoke <31307366+AbdulmalikAdegoke@users.noreply.github.com>
2022-03-06 07:27:24 +0000

    made changes to the high score page
    adds top 3

--- DuckOfFortune/DuckOfFortune/DuckOfFortune/HighScore.cs
+++ DuckOfFortune/DuckOfFortune/DuckOfFortune/HighScore.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+
 
 namespace DuckOfFortune
 {
@@ -19,17 +21,81 @@ namespace DuckOfFortune
             InitializeComponent();
         }
 
+        IDictionary<string, int> hScores = new Dictionary<string, int>();
+
         private void HighScore_Load(object sender, EventArgs e)
         {
-            for (int i = 0; i < lines.Length; i++)
+            string[] scores;
+            // Read the file and add the scores to a dictionary  
+            foreach (string line in File.ReadLines("Scores.txt"))
             {
-                label1.Text = lines[i];
+                scores = line.Split(',');
+                hScores.Add(scores[0], Int32.Parse(scores[1]));
+                Array.Clear(scores, 0, scores.Length);
             }
+            List<int> scoreNums = new List<int>();
+            //finding the FIRST, SECOND and THIRD max
+            int max=0;
+            string firstPlayer="";
+            foreach (var dict in hScores)
+            {
+                if (dict.Value > max)
+                {
+                    max = dict.Value;
+                    firstPlayer = dict.Key;
+                }
+            }
+            int secondMax = 0;
+            string secondPlayer = "";
+            foreach (var dict in hScores)
+            {
+                if (dict.Value > secondMax)
+                {
+                    if (dict.Value == max)
+                    {
+                        continue;
+                    } else
+                    {
+                        secondMax = dict.Value;
+                        secondPlayer = dict.Key;
+                    }
+                }
+            }
+            int thirdMax = 0;
+            string thirdPlayer = "";
+            foreach (var dict in hScores)
+            {
+                if (dict.Value > thirdMax)
+                {
+                    if (dict.Value == max || dict.Value == secondMax)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        thirdMax = dict.Value;
+                        thirdPlayer = dict.Key;
+                    }
+                }
+            }
+            
+
+            first.Text = "1. " + max + " by " + firstPlayer;
+
+            second.Text = "2. " + secondMax + " by " + secondPlayer;
+
+            third.Text = "3. " + thirdMax + " by " + thirdPlayer;
         }
 
 
-        private void label1_Click(object sender, EventArgs e)
+        
+
+
+        private void mainMenuButton_Click(object sender, EventArgs e)
         {
+            this.Hide();
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show();
         }
     }
 }
